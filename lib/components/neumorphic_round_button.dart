da13aa3662f3dcb057ac402408b2f39e9980bf2e
Max Walters <max.walters10@gmail.com>
2020-03-03 15:17:13 -0500

    Hold on shape, starting on luminance function

--- lib/components/neumorphic_round_button.dart
+++ lib/components/neumorphic_round_button.dart
@@ -1,24 +1,34 @@
 import 'package:flutter/material.dart';
 
-enum ButtonShape {
-  flat,
-  concave,
-  convex,
-  pressed
+// TODO: Add shape implementation
+// enum ButtonShape {
+//   flat,
+//   concave,
+//   convex,
+//   pressed
+// }
+
+colorLuminance(Color c){
+  
 }
 
 class NeumorphicRoundButton extends StatelessWidget {
   
-  NeumorphicRoundButton({@required this.shape, @required this.color, @required this.size, 
+  // TODO: Add light direction
+  NeumorphicRoundButton({@required this.mainColor, @required this.size, 
                          this.distance, this.intensity, this.blur, @required this.onPressed});
   
-  final ButtonShape shape;
-  final Color color;
+  final Color mainColor;
   final double size;
   final double distance;
   final double intensity;
   final double blur;
   final Function onPressed;
+  
+  Color grad1 = Color(0xFFffffff);
+  Color grad2 = Color(0xFFe6e6e6);
+  Color shadow1 = Color(0xFFffffff);
+  Color shadow2 = Color(0xFFC4c4c4);
   
   @override
   Widget build(BuildContext context) {
