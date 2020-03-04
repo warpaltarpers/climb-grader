f90efad254f93b184ded8d92c1219d2b287032dc
Maxwell Walters <max.walters10@gmail.com>
2020-03-03 19:36:14 -0500

    Fix issues with theme

--- lib/main.dart
+++ lib/main.dart
@@ -7,10 +7,6 @@ class ClimbGrader extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark().copyWith(
-        primaryColor: Color(0xFF0A0E21),
-        scaffoldBackgroundColor: Color(0xFF0A0E21),
-      ),
       initialRoute: '/',
       routes: {
         '/' : (context) => ResultsPage(),
