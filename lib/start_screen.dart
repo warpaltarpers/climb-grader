cdec5b37d26220dc30b6605985802eea2e4cf0c6
Maxwell Walters <max.walters10@gmail.com>
2020-03-07 16:24:04 -0500

    Remove Expanded

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -9,13 +9,11 @@ class _StartState extends State<Start> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Expanded(
-        child: Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage("lib/img/tommy-lisbin-DG-BBGw7d6E-unsplash.jpg"),
-            )
-          ),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("lib/img/tommy-lisbin-DG-BBGw7d6E-unsplash.jpg"),
+          )
         ),
       ),
     );
