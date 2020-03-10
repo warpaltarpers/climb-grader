c215c382be57a49e8d9596f765df112fa7e525de
Max Walters <max.walters10@gmail.com>
2020-03-10 15:14:11 -0400

    Got image to load, crashes randomly afterwards

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Start extends StatefulWidget {
   @override
@@ -12,7 +13,29 @@ class _StartState extends State<Start> {
       body: Container(
         decoration: BoxDecoration(
           image: DecorationImage(
-            image: AssetImage("lib/img/tommy-lisbin-DG-BBGw7d6E-unsplash.jpg"),
+            image: AssetImage('lib/img/start_bg.jpg'),
+            fit: BoxFit.cover,
+            colorFilter: ColorFilter.mode(
+                Colors.black.withOpacity(0.3),
+                BlendMode.dstATop
+            ),
+          )
+        ),
+        constraints: BoxConstraints.expand(),
+        child: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'CLIMB GRADER',
+                style: GoogleFonts.titilliumWeb(
+                  fontStyle: FontStyle.italic,
+                  fontWeight: FontWeight.w600,
+                  fontSize: 38.0
+                ),
+              ),
+            ],
           )
         ),
       ),
