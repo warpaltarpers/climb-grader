a339b3dd7fa725cb594f1ae48919c6d6566fc275
Max Walters <max.walters10@gmail.com>
2020-04-10 19:42:27 -0400

    Some UI additions

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -11,33 +11,43 @@ class _StartState extends State<Start> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'CLIMB GRADER',
-              style: GoogleFonts.titilliumWeb(
-                // fontStyle: FontStyle.italic,
-                fontWeight: FontWeight.w600,
-                fontSize: 38.0
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/bg_img.png'),
+            fit: BoxFit.cover,
+            colorFilter: ColorFilter.mode(
+                Colors.white.withOpacity(0.55), BlendMode.dstATop),
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'CLIMB GRADER',
+                style: GoogleFonts.titilliumWeb(
+                  fontStyle: FontStyle.italic,
+                  fontWeight: FontWeight.w600,
+                  fontSize: 38.0
+                ),
               ),
-            ),
-            SizedBox(
-              width: 20,
-              height: 20
-            ),
-            FlatButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/results');
-              },
-                child: Icon(
+              SizedBox(
+                width: 20,
+                height: 20
+              ),
+              IconButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/results');
+                },
+                icon: Icon(
                   AntDesign.playcircleo,
                   size: 35,
                 ),
-            ),
-          ],
+              ),
+            ],
+          ),
         ),
       )
     );
