7d361e09a67a6037596c73bb544acdd558ba5fe8
Max Walters <max.walters10@gmail.com>
2020-04-10 19:26:44 -0400

    Working on getting a working prototype of function

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_icons/flutter_icons.dart';
 
 class Start extends StatefulWidget {
   @override
@@ -10,35 +11,35 @@ class _StartState extends State<Start> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('lib/img/start_bg.jpg'),
-            fit: BoxFit.cover,
-            colorFilter: ColorFilter.mode(
-                Colors.black.withOpacity(0.3),
-                BlendMode.dstATop
-            ),
-          )
-        ),
-        constraints: BoxConstraints.expand(),
-        child: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'CLIMB GRADER',
-                style: GoogleFonts.titilliumWeb(
-                  fontStyle: FontStyle.italic,
-                  fontWeight: FontWeight.w600,
-                  fontSize: 38.0
-                ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'CLIMB GRADER',
+              style: GoogleFonts.titilliumWeb(
+                // fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.w600,
+                fontSize: 38.0
               ),
-            ],
-          )
+            ),
+            SizedBox(
+              width: 20,
+              height: 20
+            ),
+            FlatButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/results');
+              },
+                child: Icon(
+                  AntDesign.playcircleo,
+                  size: 35,
+                ),
+            ),
+          ],
         ),
-      ),
+      )
     );
   }
 }
