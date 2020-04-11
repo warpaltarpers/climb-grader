e6763aaca245bc6521cb328c12faedcf0d1d5124
Max Walters <max.walters10@gmail.com>
2020-04-10 21:28:08 -0400

    Get initial form workings implemented

--- lib/screens/form.dart
+++ lib/screens/form.dart
@@ -1,5 +1,9 @@
+import 'package:climb_grader/components/reusable_card.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import '../components/systems.dart';
+import '../components/constants.dart';
+import '../components/round_icon_button.dart';
 import 'dart:io' show Platform;
 
 class FormPage extends StatefulWidget {
@@ -12,7 +16,7 @@ class FormPage extends StatefulWidget {
 }
 
 class _FormPageState extends State<FormPage> {
-  int maxAngle = 90;
+  int maxAngle = 180;
   
   @override
   Widget build(BuildContext context) {
@@ -23,28 +27,79 @@ class _FormPageState extends State<FormPage> {
           child: Center(
             child: Column(
               children: <Widget>[
-                SliderTheme(
-                  data: SliderTheme.of(context).copyWith(
-                    thumbColor: Colors.white,
-                    overlayColor: Color(0x29FFFFFF),
-                    thumbShape: RoundSliderThumbShape(
-                      enabledThumbRadius: 15.0,
-                    ),
-                    overlayShape: RoundSliderOverlayShape(
-                      overlayRadius: 30.0,
-                    ),
-                    activeTrackColor: Colors.white,
-                    inactiveTrackColor: Color(0xFFb9e682),
-                  ),
-                  child: Slider(
-                    value: maxAngle.toDouble(),
-                    min: -45.0,
-                    max: 180.0,
-                    onChanged: (double newValue) {
-                      setState(() {
-                        maxAngle = newValue.round();
-                      });
-                    },
+                ReusableCard(
+                  cardColor: kActiveCardColor,
+                  cardChild: Column(
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.only(top: 15.0),
+                        child: Text(
+                          'MAX ANGLE',
+                          style: kLabelTextStyle,
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        textBaseline: TextBaseline.alphabetic,
+                        children: <Widget>[
+                          RoundIconButton(
+                            icon: Icons.remove,
+                            onPressed: (){
+                              setState(() {
+                                maxAngle--;
+                              });
+                            },
+                          ),
+                          SizedBox(
+                            width: 20.0,
+                          ),
+                          Text(
+                            maxAngle.toString(),
+                            style: kNumberTextStyle,
+                          ),
+                          Text(
+                            '°',
+                            style: kLabelTextStyle,
+                          ),
+                          SizedBox(
+                            width: 20.0,
+                          ),
+                          RoundIconButton(
+                            icon: Icons.add,
+                            onPressed: (){
+                              setState(() {
+                                maxAngle++;
+                              });
+                            },
+                          ),
+                        ],
+                      ),
+                      SliderTheme(
+                        data: SliderTheme.of(context).copyWith(
+                          thumbColor: Colors.white,
+                          overlayColor: Color(0x29FFFFFF),
+                          thumbShape: RoundSliderThumbShape(
+                            enabledThumbRadius: 15.0,
+                          ),
+                          overlayShape: RoundSliderOverlayShape(
+                            overlayRadius: 30.0,
+                          ),
+                          activeTrackColor: Colors.white,
+                          inactiveTrackColor: Color(0xff33c5ff),
+                        ),
+                        child: Slider(
+                          value: maxAngle.toDouble(),
+                          min: 90.0,
+                          max: 270.0,
+                          onChanged: (double newValue) {
+                            setState(() {
+                              maxAngle = newValue.round();
+                            });
+                          },
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ],
