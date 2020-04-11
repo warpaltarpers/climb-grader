a4853d1bae306a835369c761842e6593cc86505d
Max Walters <max.walters10@gmail.com>
2020-04-10 20:22:22 -0400

    Reorganizing

--- lib/screens/start_screen.dart
+++ lib/screens/start_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_icons/flutter_icons.dart';
-import 'components/systems.dart' as sys;
+import '../components/systems.dart' as sys;
 
 class Start extends StatefulWidget {
   @override
