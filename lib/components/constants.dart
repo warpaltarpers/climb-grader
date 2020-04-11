d5bda598ca314c653750062e23dd102faee4d811
Max Walters <max.walters10@gmail.com>
2020-04-10 20:57:53 -0400

    Working on adding components and form fnctn

---
+++ lib/components/constants.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+const kCardTextColor = Color(0xff111111);
+
+const kCardTextStyle = TextStyle(
+  fontSize: 18.0,
+  color: kCardTextColor,
+);
+
+const kLabelTextStyle = TextStyle(
+  fontSize: 18.0,
+  color: kCardTextColor,
+  fontWeight: FontWeight.w900,
+);
+
+const kNumberTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.w900,
+  color: kCardTextColor,
+);
+
+const kActiveCardColor = Color(0xa04ccdff);
+const kInactiveCardColor = Color(0xa07FDBFF);
--- lib/screens/form.dart
+++ lib/screens/form.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import '../components/systems.dart';
+import 'dart:io' show Platform;
 
 class FormPage extends StatefulWidget {
-  FormPage({@required this.gradeSystem});
+  FormPage({this.gradeSystem});
   
   final GradingSystem gradeSystem;
   
@@ -11,10 +12,46 @@ class FormPage extends StatefulWidget {
 }
 
 class _FormPageState extends State<FormPage> {
+  int maxAngle = 90;
+  
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Container(
+        child: SafeArea(
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                SliderTheme(
+                  data: SliderTheme.of(context).copyWith(
+                    thumbColor: Colors.white,
+                    overlayColor: Color(0x29FFFFFF),
+                    thumbShape: RoundSliderThumbShape(
+                      enabledThumbRadius: 15.0,
+                    ),
+                    overlayShape: RoundSliderOverlayShape(
+                      overlayRadius: 30.0,
+                    ),
+                    activeTrackColor: Colors.white,
+                    inactiveTrackColor: Color(0xFFb9e682),
+                  ),
+                  child: Slider(
+                    value: maxAngle.toDouble(),
+                    min: -45.0,
+                    max: 180.0,
+                    onChanged: (double newValue) {
+                      setState(() {
+                        maxAngle = newValue.round();
+                      });
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
