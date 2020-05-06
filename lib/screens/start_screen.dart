116ea2abacb59b4d8a200facf1e10594ad79fa91
Max Walters <max.walters10@gmail.com>
2020-05-06 17:31:14 -0400

    Design and message hard-coded

--- lib/screens/start_screen.dart
+++ lib/screens/start_screen.dart
@@ -26,59 +26,61 @@ class _StartState extends State<Start> {
             colors: kColorGradient
           )
         ),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'CLIMB GRADER',
-                style: GoogleFonts.lato(
-                  fontStyle: FontStyle.normal,
-                  fontWeight: FontWeight.w400,
-                  fontSize: 38.0,
-                  color: Color(0xFFFFFFFF)
+        child: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'CLIMB GRADER',
+                  style: GoogleFonts.lato(
+                    fontStyle: FontStyle.normal,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 38.0,
+                    color: Color(0xFFFFFFFF)
+                  ),
                 ),
-              ),
-              SizedBox(
-                width: 20,
-                height: 40
-              ),
-              CarouselSelect(
-                valueList: sys.systems,
-                onChanged: (String newSystem) {
-                  setState(() {
-                    system = newSystem;
-                  });
-                },
-                initialPosition: 0,
-                scrollDirection: ScrollDirection.horizontal,
-                activeItemFontSize: 25.0,
-                activeItemTextColor: Color(0xFFFFFFFF),
-                passiveItemFontSize: 20.0,
-                passiveItemsTextColor: Color(0xFFFFFFFF).withOpacity(0.7),
-                backgroundColor: Color(0xFFFFFFFF).withOpacity(0.0),
-                height: 50.0,
-              ),
-              SizedBox(
-                width: 20,
-                height: 30
-              ),
-              IconButton(
-                onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(
-                    builder: (context) => FormPage(
-                      gradeSystem: parseSystem(system),
-                    )
-                  ));
-                },
-                icon: Icon(
-                  Entypo.controller_play,
-                  color: Color(0xFFFFFFFF),
-                  size: 35,
+                SizedBox(
+                  width: 20,
+                  height: 40
                 ),
-              ),
-            ],
+                CarouselSelect(
+                  valueList: sys.systems,
+                  onChanged: (String newSystem) {
+                    setState(() {
+                      system = newSystem;
+                    });
+                  },
+                  initialPosition: 0,
+                  scrollDirection: ScrollDirection.horizontal,
+                  activeItemFontSize: 25.0,
+                  activeItemTextColor: Color(0xFFFFFFFF),
+                  passiveItemFontSize: 20.0,
+                  passiveItemsTextColor: Color(0xFFFFFFFF).withOpacity(0.7),
+                  backgroundColor: Color(0xFFFFFFFF).withOpacity(0.0),
+                  height: 50.0,
+                ),
+                SizedBox(
+                  width: 20,
+                  height: 30
+                ),
+                IconButton(
+                  onPressed: () {
+                    Navigator.push(context, MaterialPageRoute(
+                      builder: (context) => FormPage(
+                        gradeSystem: parseSystem(system),
+                      )
+                    ));
+                  },
+                  icon: Icon(
+                    Entypo.controller_play,
+                    color: Color(0xFFFFFFFF),
+                    size: 35,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       )
